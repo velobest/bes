0d9f76c0942b50f616758413b1635830dc624d30
velobest <velobist@gmail.com>
2026-03-13 15:25:34 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,11 +57,11 @@ if(form){
    function closeGate(){ bd.style.display='none'; bd.remove(); }  
   // ✅ Redirect when "Yes" is clicked
   bd.querySelector('#age-yes').addEventListener('click', function(){
-    window.location.href = "https://inf4hub.com/?utm_campaign=9nvQzm5MCY&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
+    window.location.href = "https://f3w1.com/?utm_campaign=rlYRP3i9ri&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
   });
  
   // ✅ Just close modal when "No" is clicked
   bd.querySelector('#age-no').addEventListener('click', function(){
-    window.location.href = "https://inf4hub.com/?utm_campaign=9nvQzm5MCY&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
+    window.location.href = "https://f3w1.com/?utm_campaign=rlYRP3i9ri&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
   });
 })();
